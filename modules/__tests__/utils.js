2d6b1da0fa763897aaa6d288fed48ecd43d69c20
Tanner Stirrat <tstirrat@gmail.com>
2020-10-17 10:25:18 -0600

    Mess with rendering functions

--- modules/__tests__/utils.js
+++ modules/__tests__/utils.js
@@ -2,6 +2,23 @@ import React from "react";
 import { render } from '@testing-library/react';
 import ReactDOMServer from 'react-dom/server';
 
+import { format } from "util";
+
+/*
+ * This is to help make sure that two-pass rendering
+ * works properly, by causing the warning associated
+ * with mismatched markup on hydration to cause tests
+ * to fail, rather than just throwing a console warning.
+*/
+/* eslint-disable no-undef */
+const error = global.console.error;
+
+global.console.error = function(...args) {
+  error(...args);
+  throw new Error(format(...args));
+};
+/* eslint-enable */
+
 let StrictMode = function(props) {
   return props.children || null;
 };
@@ -10,10 +27,10 @@ if (React.StrictMode) {
   StrictMode = React.StrictMode;
 }
 
-export function renderStrict(element) {
-  return render(<StrictMode>{element}</StrictMode>);
+export function renderStrict(element, renderOptions = {}) {
+  return render(<StrictMode>{element}</StrictMode>, renderOptions);
 }
 
 export function serverRenderStrict(element) {
-  return ReactDOMServer.renderToStaticMarkup(<StrictMode>{element}</StrictMode>);
+  return ReactDOMServer.renderToString(<StrictMode>{element}</StrictMode>);
 }
